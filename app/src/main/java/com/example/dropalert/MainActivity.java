8706c3dce1d8fe8c5df7c94f5aa663ed35f3c074
Michalis <57771939+MichelStath@users.noreply.github.com>
2023-03-16 03:10:35 +0200

    service

--- app/src/main/java/com/example/dropalert/MainActivity.java
+++ app/src/main/java/com/example/dropalert/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.dropalert;
 
+import static android.app.Service.STOP_FOREGROUND_REMOVE;
+import static androidx.core.app.ServiceCompat.stopForeground;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ServiceCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.ActivityManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -37,16 +49,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkState() {
+        Intent serviceIntent = new Intent(this, MyForegroundService.class);
         if(switchCompat.isChecked()){
             Log.i("switch","checked");
             stateTV.setText(STATE_ON_STRING);
             stateTV.setTextColor(getResources().getColor(R.color.green));
-
+            if(!foregroundServiceRunning()) {
+                //runs forever
+                startForegroundService(serviceIntent);
+            }
         }else {
             Log.i("switch", "not Checked");
             stateTV.setText(STATE_OFF_STRING);
             stateTV.setTextColor(getResources().getColor(R.color.red));
+            stopService(serviceIntent);
         }
 
     }
+
+    public boolean foregroundServiceRunning(){
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for(ActivityManager.RunningServiceInfo service: activityManager.getRunningServices(Integer.MAX_VALUE)) {
+            if(MyForegroundService.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/dropalert/MyForegroundService.java
@@ -0,0 +1,67 @@
+package com.example.dropalert;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MyForegroundService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            Log.i("Service", "Service is running...");
+                            try {
+                                Thread.sleep(2000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+        ).start();
+
+        final String CHANNELID = "Foreground Service ID";
+        NotificationChannel channel = new NotificationChannel(
+                CHANNELID,
+                CHANNELID,
+                NotificationManager.IMPORTANCE_LOW
+        );
+
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        Notification.Builder notification = new Notification.Builder(this, CHANNELID)
+                .setContentText("Service is running")
+                .setContentTitle("Service enabled")
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
+        startForeground(1001, notification.build());
+
+
+
+
+        return START_NOT_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("Service","STOPED");
+        super.onDestroy();
+    }
+    
+}
